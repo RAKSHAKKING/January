472d708906b2b2a36ce4dca99d09c1a4e355ab0b
Rakshak Kumar <kingrakshak199@gmail.com>
2024-01-03 09:27:12 +0530

    added the files

---
+++ index.js
@@ -0,0 +1,68 @@
+/*console.log("hey bro", 88,"again hello", "hello fucker");
+alert("mistake h yeh be");
+console.warn("bhai maat kr yeh kyu nhi samj rha h");
+console.warn("fuck u");
+console.warn("bhai kya rha h padh le 3 gante");
+console.error("bro don't pass any more time cuz it's u ");
+console.error("who's gonna suffer the most");
+console.error("this is an fuckin error");
+*/
+
+var num1 = 8;
+var num2 = 7;
+var total  = num1 + num2;
+// console.log(total);
+
+var str = "new string";
+var strr = "new string is here";
+
+var javaMarks = {
+    rakshak:90,
+    shubham:89,
+    sunil:99
+}
+
+// console.log(javaMarks);
+
+
+let arr = [2,3,4,5,6,"rakshak","hey2guys",true];
+// console.log(arr);
+
+let a = 55;
+let b = 10;
+
+// console.log("the value of ", a ,"+" ,b , "is", a+b);
+// console.log("the value of ", a ,"-" ,b , "is", a-b);
+// console.log("the value of ", a ,"*" ,b , "is", a*b);
+// console.log("the value of ", a ,"/" ,b , "is", a/b);
+// console.log("the value of ", a ,"%" ,b , "is", a&b);
+
+// c = b;
+// console.log(c);
+
+// c+=10;
+// console.log(c);
+
+// c-=10;
+// console.log(c);
+
+// c*=10;
+// console.log(c);
+
+// c/=10
+// console.log(c);
+
+console.log(a==b);
+console.log(a>=b);
+console.log(a<=b);
+// console.log(a=<b); this is SyntaxError
+
+// console.log(true || true);
+// console.log(true || false);
+// console.log(false || true);
+// console.log(false || false);
+
+
+
+
+
